11a1d8eaba7ca11174a3585b7134000fed2e36cb
CandaceAndrews <candacewolfandrews@hotmail.com>
2023-09-01 16:36:44 -0400

    basic set-up

--- webtoon-app/models.py
+++ webtoon-app/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    pass
+
+
+class Chapter(models.Model):
+    title = models.CharField(max_length=200)
+    image = models.ImageField(upload_to='chapter_images/')
+    summary = models.TextField()
+    date_published = models.DateTimeField(auto_now_add=True)
+
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    chapter = models.ForeignKey(Chapter, on_delete=models.CASCADE)
+    text = models.TextField()
+    date_posted = models.DateTimeField(auto_now_add=True)
