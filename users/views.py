8614d5a309a08dfea827ddeefd784a0b60be1b76
CandaceAndrews <candacewolfandrews@hotmail.com>
2023-10-23 19:29:12 -0400

    UserDetailView

--- users/views.py
+++ users/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import User
 
-# Create your views here.
+
+class UserDetailView(generics.RetrieveAPIView):
+    ''' view details for single user
+    '''
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    lookup_field = 'username'
