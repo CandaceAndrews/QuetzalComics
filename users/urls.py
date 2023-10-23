441e7783ecd8b11fae5ee4ad4f0b5c28dcc6745c
CandaceAndrews <candacewolfandrews@hotmail.com>
2023-10-23 19:54:28 -0400

    UserDetailView, user.urls

--- users/urls.py
+++ users/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from users import views
 
 urlpatterns = [
-    path('user/<str:username>', UserDetailView.as_view(), name='user-detail'),
+    path('users/<str:username>/', views.UserDetailView.as_view(), name='user-detail'),
 ]
--- users/views.py
+++ users/views.py
@@ -3,6 +3,7 @@ from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.authentication import TokenAuthentication
 from .models import User
+from .serializers import UserSerializer
 
 
 class UserDetailView(generics.RetrieveAPIView):
