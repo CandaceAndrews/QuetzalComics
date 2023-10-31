21971c922dde24350fb64083ee75490c04a0c113
CandaceAndrews <candacewolfandrews@hotmail.com>
2023-10-30 22:10:40 -0400

    Edited ProfileSerializer to display names of followers instead of pk

--- webcomic_app/serializers.py
+++ webcomic_app/serializers.py
@@ -3,12 +3,23 @@ from .models import Series
 
 
 class SeriesSerializer(serializers.ModelSerializer):
+    creator_username = serializers.SerializerMethodField()
+    follower_usernames = serializers.SerializerMethodField()
+
     class Meta:
         model = Series
         fields = (
             "title",
             "description",
             "cover_image",
-            "creator",
-            "followers",
+            "creator_username",
+            "follower_usernames",
         )
+
+    def get_creator_username(self, obj):
+        return obj.creator.username  # Extract the username from the related User object
+
+    def get_follower_usernames(self, obj):
+        follower_usernames = [
+            follower.username for follower in obj.followers.all()]
+        return follower_usernames
