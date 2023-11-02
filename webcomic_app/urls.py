b3813059c8c60c7b7e7773c225148f76820a2979
CandaceAndrews <candacewolfandrews@hotmail.com>
2023-11-02 01:13:48 -0400

    added taggit so Series can have tags

--- webcomic_app/urls.py
+++ webcomic_app/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.api_root),
     path("series/<int:pk>/", views.SeriesDetailView.as_view(), name='series-detail'),
+    path('series/tag/<str:tag_name>/',
+         views.SeriesByTagListView.as_view(), name='series-by-tag')
 ]
--- webcomic_app/views.py
+++ webcomic_app/views.py
@@ -3,6 +3,9 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.authentication import TokenAuthentication
+from django.views.generic.list import ListView
+from django.shortcuts import get_object_or_404
+from taggit.models import Tag
 
 from .models import Chapter, Comment, Series
 from .serializers import SeriesSerializer
@@ -26,3 +29,12 @@ class SeriesDetailView(generics.RetrieveAPIView):
     # permission_classes = [IsAuthenticated]
     queryset = Series.objects.all()
     serializer_class = SeriesSerializer
+
+
+class SeriesByTagListView(ListView):
+    serializer_class = SeriesSerializer
+
+    def get_queryset(self):
+        tag_name = self.kwargs['tag_name']
+        tag = get_object_or_404(Tag, name=tag_name)
+        return Series.objects.filter(tags=tag)
