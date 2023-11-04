efa74d1142f44102247fa9b51280b93bad020955
CandaceAndrews <candacewolfandrews@hotmail.com>
2023-11-03 23:45:04 -0400

    Organized backend files into the 'backend' folder

--- users/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from users import views
-
-urlpatterns = [
-    path('user/<str:username>/', views.UserDetailView.as_view(), name='user-detail'),
-    path('user/profile/<str:user__username>/',
-         views.ProfileDetailView.as_view(), name='user-profile-detail')
-]
--- webcomic_app/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class WebcomicAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'webcomic_app'
